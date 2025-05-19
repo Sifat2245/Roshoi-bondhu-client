62d7f3ada16a44b8ef2d92eee8272dd2618488cc
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-05-20 04:10:28 +0600

    project setup. ready to code

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className='text-6xl'> welcome to thegfsdf sdfcfgggggggg roshoibondhu </h1>
+        </div>
+    );
+};
+
+export default MainLayout;
